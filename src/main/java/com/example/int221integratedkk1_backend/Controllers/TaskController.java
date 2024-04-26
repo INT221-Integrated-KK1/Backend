54be051a078beb3cf8393b5dcee3a135cdde5bb4
MookizsHub <mookies2004@gmail.com>
2024-04-26 19:32:37 +0700

    modift DTO

--- src/main/java/com/example/int221integratedkk1_backend/Controllers/TaskController.java
+++ src/main/java/com/example/int221integratedkk1_backend/Controllers/TaskController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
-
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/v1/tasks")
 public class TaskController {
--- src/main/java/com/example/int221integratedkk1_backend/DTOS/TaskDTO.java
+++ src/main/java/com/example/int221integratedkk1_backend/DTOS/TaskDTO.java
@@ -1,9 +1,14 @@
 package com.example.int221integratedkk1_backend.DTOS;
 
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.sql.Timestamp;
+
 @Setter
 @Getter
 @NoArgsConstructor
@@ -15,9 +20,15 @@ public class TaskDTO {
 
     private String taskTitle;
 
+    private String taskDescription;
+
     private String taskAssignees;
 
     private Object taskStatus;
+
+    private Timestamp createdOn;
+
+    private Timestamp updatedOn;
 
 
 }
